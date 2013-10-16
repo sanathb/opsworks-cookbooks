ff817774b31639f23d522803bb9abfa5effbc212
iwaltgen <iwaltgen@gmail.com>
2013-10-16 15:43:56 +0900

    setup 레시피에 unzip 추가 작업. 서비스 스크립트 수정 작업.

--- spray/recipes/setup.rb
+++ spray/recipes/setup.rb
@@ -1,3 +1,7 @@
+package 'unzip' do
+  action :install
+end
+
 template 'spray configuration' do
   path value_for_platform(
     ['centos','redhat','fedora','amazon'] => {'default' => '/etc/init.d/spray'},
