dfaa67d768ca04f63c8c62fd55dd511f71487be8
iwaltgen <iwaltgen@gmail.com>
2013-10-16 21:17:11 +0900

    spray [deploy, undeploy] 레시피 수정 작업. - ubuntu 기반으로 작성.

--- spray/recipes/deploy.rb
+++ spray/recipes/deploy.rb
@@ -1,15 +1,35 @@
+include_recipe 'deploy'
+
 zip_filename = node[:spray][:app][:name] + '.zip'
 zip_filepath = node[:spray][:path] + '/' + zip_filename
 
-bash 'move & unzip #{zip_filename} file' do
-  cwd node[:spray][:path]
-  user 'root'
-  code <<-EOH
-  mv -f ~/home/#{zip_filename} ./
-  unzip -o ./#{zip_filename}
-  mv -rf ./#{node[:spray][:app][:name] + '-' + node[:spray][:app][:version]}/* ./
-  rm -rf ./#{node[:spray][:app][:name] + '-' + node[:spray][:app][:version]}
-  /etc/init.d #{node[:spray][:app][:name]} restart
-  EOH
-  only_if { ::File.exists?('~/home/' + zip_filepath) }
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'custom'
+    Chef::Log.debug("Skipping deploy::custom application #{application} as it is not an custom spray app")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    app application
+    deploy_data deploy
+  end
+
+  bash 'move & unzip #{zip_filename} file' do
+  	cwd node[:spray][:path]
+  	user 'root'
+  	code <<-EOH
+  	mv -f ~/home/#{zip_filename} ./
+  	unzip -o ./#{zip_filename}
+  	mv -rf ./#{node[:spray][:app][:name] + '-' + node[:spray][:app][:version]}/* ./
+  	rm -rf ./#{node[:spray][:app][:name] + '-' + node[:spray][:app][:version]}
+  	/etc/init.d #{node[:spray][:app][:name]} restart
+  	EOH
+  	only_if { ::File.exists?('~/home/' + zip_filepath) }
+  end
 end
