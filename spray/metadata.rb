826f12e8d301488a4fe49493d09cf95f7cd81609
iwaltgen <iwaltgen@gmail.com>
2013-10-17 01:52:05 +0900

    메타데이터 수정. - 레시피 기술. - 지원 가능 OS 기술. - 의존 관계 기술.

--- spray/metadata.rb
+++ spray/metadata.rb
@@ -5,3 +5,11 @@ license          'Apache 2.0'
 description      'Installs/Configures spray-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe "spray::setup", "Installs spray service script"
+recipe "spray::deploy", "Download & restart service"
+recipe "spray::undeploy", "stop service"
+
+supports "ubuntu"
+
+depends "aws"
