9f385ab561fc22b5b9980e6d31a20aa45609c46f
iwaltgen <iwaltgen@gmail.com>
2013-10-17 13:26:04 +0900

    .

--- spray/recipes/deploy.rb
+++ spray/recipes/deploy.rb
@@ -16,8 +16,8 @@ bash 'unzip #{zip_filename} file' do
 	code <<-EOH
 	/etc/init.d/#{node[:spray][:name]} stop
 	chmod 755 ./#{zip_filename}
-  unzip ./#{zip_filename}
-  rm -rf ./#{node[:spray][:app][:name]}
+  	unzip ./#{zip_filename}
+  	rm -rf ./#{node[:spray][:app][:name]}
 	mv ./#{node[:spray][:app][:name] + '-' + node[:spray][:app][:version]} ./#{node[:spray][:app][:name]}
 	rm -rf ./#{zip_filename}
 	/etc/init.d/#{node[:spray][:name]} start
