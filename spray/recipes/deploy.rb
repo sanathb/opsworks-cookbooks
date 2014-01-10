390b65915b6a0cb298a5d196ab6ec92b5e06f284
iwaltgen <iwaltgen@gmail.com>
2014-01-10 12:30:15 +0900

    spray deploy recipe 수정. - 정지/시작 시점을 분리.

--- spray/recipes/deploy.rb
+++ spray/recipes/deploy.rb
@@ -4,6 +4,14 @@ include_recipe "spray::setup"
 filename = node['spray']['app']['name'] + '.jar'
 filepath = node['spray']['path'] + '/' + filename
 
+bash 'stop #{filename}' do
+  cwd node['spray']['path']
+  user 'root'
+  code <<-EOH
+    /etc/init.d/#{node['spray']['name']} stop
+  EOH
+end
+
 aws_s3_file filepath do
   bucket node['aws']['bucket_name']
   remote_path filename
@@ -11,11 +19,10 @@ aws_s3_file filepath do
   aws_secret_access_key node['aws']['secret_access_key']
 end
 
-bash 'restart #{filename}' do
+bash 'start #{filename}' do
   cwd node['spray']['path']
   user 'root'
   code <<-EOH
-    /etc/init.d/#{node['spray']['name']} stop
     /etc/init.d/#{node['spray']['name']} start
   EOH
 end
