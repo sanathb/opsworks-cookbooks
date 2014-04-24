5199e69e9804c7d556893b70087b0d25adae3d7c
iwaltgen <iwaltgen@gmail.com>
2014-04-24 17:07:53 +0900

    newrelic 설정 추가.

--- spray/attributes/default.rb
+++ spray/attributes/default.rb
@@ -2,6 +2,7 @@
 default['spray']['name'] = 'spray'
 default['spray']['path'] = '/var/' + default['spray']['name']
 
-default['spray']['java']['option'] = '-Dfile.encoding=UTF-8 -J-server -mem=1024'
+default['spray']['java']['option'] = '-Dfile.encoding=UTF-8 -J-server -mem 1024'
 default['spray']['app']['name'] = 'app'
 default['spray']['app']['version'] = '1.0.0'
+default['spray']['app']['newrelic'] = false
--- spray/recipes/setup.rb
+++ spray/recipes/setup.rb
@@ -1,3 +1,8 @@
+include_recipe "aws"
+
+package 'unzip' do
+  action :install
+end
 
 template '/etc/init.d/spray' do
   source 'spray.erb'
@@ -11,4 +16,23 @@ directory node['spray']['path'] do
   group 'root'
   mode 0755
   recursive true
+end
+
+newrelic_filename = node['spray']['app']['name'] +'-' + 'newrelic-agent.zip'
+
+aws_s3_file '/var/' + newrelic_filename do
+  only_if node['spray']['app']['newrelic']
+  bucket node['aws']['bucket_name']
+  remote_path newrelic_filename
+  aws_access_key_id node['aws']['access_key_id']
+  aws_secret_access_key node['aws']['secret_access_key']
+end
+
+bash 'unzip newrelic ' do
+  only_if node['spray']['app']['newrelic']
+  cwd '/var'
+  user 'root'
+  code <<-EOH
+    tar -zxvf ./#{newrelic_filename}
+  EOH
 end
